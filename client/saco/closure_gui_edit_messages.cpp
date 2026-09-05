af47f855229c545713470ac82fa9f99593c8808a
Dryxio <momemtumcorpfzco@gmail.com>
2026-09-05 20:42:30 +0100

    Record bounded EditBox argument trials without coverage credit

--- client/saco/closure_gui_edit_messages.cpp
+++ client/saco/closure_gui_edit_messages.cpp
@@ -106,6 +106,7 @@ bool CDXUTEditBox::MsgProc( UINT uMsg, WPARAM wParam, LPARAM lParam )
 
 					if(strlen(GetTextA()) >= 128) return true;
 
+                    int nIndex;
                     bool bPlaceCaret = false;
                     if( !m_bInsertMode && m_nCaret < m_Buffer.GetTextSize() )
                     {
@@ -120,9 +121,11 @@ bool CDXUTEditBox::MsgProc( UINT uMsg, WPARAM wParam, LPARAM lParam )
                     }
                     else
                     {
-                        bPlaceCaret = (WCHAR)wParam > 255 ?
-                            m_Buffer.InsertChar(m_nCaret, (WCHAR)wParam) :
-                            m_Buffer.InsertChar(m_nCaret, (CHAR)wParam);
+                        nIndex = m_nCaret;
+                        if( (WCHAR)wParam > 255 )
+                            bPlaceCaret = m_Buffer.InsertChar(nIndex, (WCHAR)wParam);
+                        else
+                            bPlaceCaret = m_Buffer.InsertChar(nIndex, (CHAR)wParam);
                     }
                     if( bPlaceCaret )
                     {
@@ -147,67 +150,4 @@ bool CDXUTEditBox::MsgProc( UINT uMsg, WPARAM wParam, LPARAM lParam )
 		}
     }
     return false;
-}
-bool CUniBuffer::InsertChar( int nIndex, WCHAR tchr )
-{
-    assert( nIndex >= 0 );
-
-    if( nIndex < 0 || nIndex > lstrlenW( m_pwszBuffer ) )
-        return false;  // invalid index
-
-    // Check for maximum length allowed
-    if( GetTextSize() + 1 >= DXUT_MAX_EDITBOXLENGTH )
-        return false;
-
-    if( lstrlenW( m_pwszBuffer ) + 1 >= m_nBufferSize )
-    {
-        if( !SetBufferSize( -1 ) )
-            return false;  // out of memory
-    }
-
-    assert( m_nBufferSize >= 2 );
-
-    // Shift the characters after the index, start by copying the null terminator
-    WCHAR* dest = m_pwszBuffer + lstrlenW(m_pwszBuffer)+1;
-    WCHAR* stop = m_pwszBuffer + nIndex;
-    WCHAR* src = dest - 1;
-
-    while( dest > stop )
-    {
-        *dest-- = *src--;
-    }
-
-    // Set new character
-    m_pwszBuffer[ nIndex ] = tchr;
-    m_bAnalyseRequired = true;
-
-    return true;
-}
-bool CUniBuffer::InsertChar(int nIndex, CHAR tchr)
-{
-	WCHAR WideCharStr[2];
-	ZeroMemory(&WideCharStr[0], sizeof(WideCharStr));
-
-	if(field_8)
-	{
-		CHAR MultiByteStr[3];
-		MultiByteStr[0] = field_8;
-		MultiByteStr[1] = tchr;
-		MultiByteStr[2] = 0;
-		MultiByteToWideChar(CP_ACP, 0, MultiByteStr, 2, WideCharStr, 1);
-		InsertChar(nIndex, WideCharStr[0]);
-		field_8 = 0;
-		return true;
-	}
-	else if(IsDBCSLeadByteEx(CP_ACP, tchr))
-	{
-		field_8 = tchr;
-		return false;
-	}
-	else
-	{
-		MultiByteToWideChar(CP_ACP, 0, &tchr, 1, WideCharStr, 1);
-		InsertChar(nIndex, WideCharStr[0]);
-		return true;
-	}
 }
--- tools/prepare_gui_edit_messages.py
+++ tools/prepare_gui_edit_messages.py
@@ -5,7 +5,8 @@ def prepare():
  s=(ROOT/'vendor/upstream/saco/d3d9/common/DXUTgui.cpp').read_text();b=definition(s,'CDXUTEditBox::MsgProc');start=b.index('\t\t\t\t\tbool bPlaceCaret;');end=b.index('                    ResetCaretBlink();',start)
  update='''                            PlaceCaret( m_nCaret + 1 );
                             m_nSelStart = m_nCaret;'''
- replacement="""                    bool bPlaceCaret = false;
+ replacement="""                    int nIndex;
+                    bool bPlaceCaret = false;
                     if( !m_bInsertMode && m_nCaret < m_Buffer.GetTextSize() )
                     {
                         if( (WCHAR)wParam > 255 )
@@ -18,9 +19,11 @@ def prepare():
                     }
                     else
                     {
-                        bPlaceCaret = (WCHAR)wParam > 255 ?
-                            m_Buffer.InsertChar(m_nCaret, (WCHAR)wParam) :
-                            m_Buffer.InsertChar(m_nCaret, (CHAR)wParam);
+                        nIndex = m_nCaret;
+                        if( (WCHAR)wParam > 255 )
+                            bPlaceCaret = m_Buffer.InsertChar(nIndex, (WCHAR)wParam);
+                        else
+                            bPlaceCaret = m_Buffer.InsertChar(nIndex, (CHAR)wParam);
                     }
                     if( bPlaceCaret )
                     {
@@ -28,5 +31,5 @@ def prepare():
                     }
 """
  b=(b[:start]+replacement+b[end:]).replace('(TCHAR)wParam','(WCHAR)wParam')
- (ROOT/'client/saco/closure_gui_edit_messages.cpp').write_text('#include "d3d9/common/dxstdafx.h"\n#define DXUT_MAX_EDITBOXLENGTH 0xFFFF\n'+b+'\n'+'\n'.join(definition(s,'CUniBuffer::InsertChar',i) for i in range(2))+'\n')
+ (ROOT/'client/saco/closure_gui_edit_messages.cpp').write_text('#include "d3d9/common/dxstdafx.h"\n#define DXUT_MAX_EDITBOXLENGTH 0xFFFF\n'+b+'\n')
 if __name__=='__main__':prepare()
