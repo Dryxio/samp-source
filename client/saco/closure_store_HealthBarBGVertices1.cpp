efe6c8893500066987ad8b3dc8d7cacb9f0ced23
Dryxio <momemtumcorpfzco@gmail.com>
2026-09-06 04:32:04 +0100

    Match TEA and health rendering: 3330 unique bytes

---
+++ client/saco/closure_store_HealthBarBGVertices1.cpp
@@ -0,0 +1,9 @@
+// One complete actual80-byte vertex array, minimal includes.
+#include "newplayertags.h"
+HealthBarVertices1_s HealthBarBGVertices1[4] =
+{	//x     y      z     rhw   c
+	{2.0f,  2.0f,  0.0f, 1.0f, D3DCOLOR_XRGB(75, 11, 20)},
+	{2.0f,  18.0f, 0.0f, 1.0f, D3DCOLOR_XRGB(75, 11, 20)},
+	{96.0f, 18.0f, 0.0f, 1.0f, D3DCOLOR_XRGB(75, 11, 20)},
+	{96.0f, 2.0f,  0.0f, 1.0f, D3DCOLOR_XRGB(75, 11, 20)}
+};
---
+++ client/saco/closure_store_matWorld.cpp
@@ -0,0 +1,2 @@
+#include <d3dx9.h>
+D3DXMATRIX matWorld; // realBSS140DF0/64, SetTransform state256
